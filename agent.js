b990abf706ac1228b32335b108cfd7a4a213c108
Puran Singh Rana <puransinghrana080@gmail.com>
2025-11-04 19:51:59 +0530

    first question ask

--- agent.js
+++ agent.js
@@ -1,1 +1,22 @@
-console.log("Hello from agent")
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function callAgent(){
+    const completion =await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Who are you ?",
+      },
+    ],
+    model: "openai/gpt-oss-20b",
+  });
+
+  console.log(JSON.stringify(completion.choices[0],null,2))
+   
+
+}
+
+callAgent()
+

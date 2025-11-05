33aab97d2c20aa4ae0601488370ff2e972c7ce15
Puran Singh Rana <puransinghrana080@gmail.com>
2025-11-05 12:02:52 +0530

    add user prompt like chat

--- agent.js
+++ agent.js
@@ -1,106 +1,117 @@
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
 const expenseDB = [];
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function callAgent() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
   const messages = [
     {
       role: "system",
       content: `You are Josh, a personel finance assistant. Your task is to assist user with their expenses, balances and financial planning.
+      You have access to following tools:
+      1. getTotalExpense({from,to}):string //get total expense for a time period
+      2. addExpense({name,amount}):string  //add new expense to the expense db
         currnt datetime: ${new Date().toUTCString()}`,
     },
   ];
-  messages.push({
-    role: "user",
-    content: "hey , i just bought mac m1 for 60000 inr",
-  });
 
+  //   this is for user prompt loop
   while (true) {
-    const completion = await groq.chat.completions.create({
-      messages: messages,
-      model: "openai/gpt-oss-20b",
-      tools: [
-        {
-          type: "function",
-          function: {
-            name: "getTotalExpense",
-            description: "Get total expense from date to date",
-            parameters: {
-              type: "object",
-              properties: {
-                from: {
-                  type: "string",
-                  description: "from date to get the expense",
-                },
-                to: {
-                  type: "string",
-                  description: "to date to get expense",
-                },
-              },
-            },
-          },
-        },
-        {
-          type: "function",
-          function: {
-            name: "addExpense",
-            description: "Add new expense entry to the expense database",
-            parameters: {
-              type: "object",
-              properties: {
-                name: {
-                  type: "string",
-                  description:
-                    "Name of the expense. e.g. bought an iphone for 5000 inr",
-                },
-                amount: {
-                  type: "string",
-                  description: "Amount of the expense",
-                },
-              },
-            },
-          },
-        },
-      ],
-    });
-
-    messages.push(completion.choices[0].message);
-
-    const toolCalls = completion.choices[0].message.tool_calls;
-    if (!toolCalls) {
-      console.log(`Assistant: ${completion.choices[0].message.content}`);
+    const question = await rl.question("User: ");
+    if (question === "bye") {
       break;
     }
-    for (const tool of toolCalls) {
-      const functionName = tool.function.name;
-      const functionArgs = tool.function.arguments;
-
-      let result = "";
-      if (functionName === "getTotalExpense") {
-        result = getTotalExpense(JSON.parse(functionArgs));
-      } else if (functionName === "addExpense") {
-        result = addExpense(JSON.parse(functionArgs));
-      }
-      messages.push({
-        role: "tool",
-        content: result,
-        tool_call_id: tool.id,
+    messages.push({
+      role: "user",
+      content: question,
+    });
+    // this is for agent
+    while (true) {
+      const completion = await groq.chat.completions.create({
+        messages: messages,
+        model: "openai/gpt-oss-20b",
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "getTotalExpense",
+              description: "Get total expense from date to date",
+              parameters: {
+                type: "object",
+                properties: {
+                  from: {
+                    type: "string",
+                    description: "from date to get the expense",
+                  },
+                  to: {
+                    type: "string",
+                    description: "to date to get expense",
+                  },
+                },
+              },
+            },
+          },
+          {
+            type: "function",
+            function: {
+              name: "addExpense",
+              description: "Add new expense entry to the expense database",
+              parameters: {
+                type: "object",
+                properties: {
+                  name: {
+                    type: "string",
+                    description:
+                      "Name of the expense. e.g. bought an iphone for 5000 inr",
+                  },
+                  amount: {
+                    type: "string",
+                    description: "Amount of the expense",
+                  },
+                },
+              },
+            },
+          },
+        ],
       });
-      console.log("===========================");
-      console.log("Messages", messages);
-      console.log("===========================");
-      console.log("DB: ", expenseDB);
+
+      messages.push(completion.choices[0].message);
+
+      const toolCalls = completion.choices[0].message.tool_calls;
+      if (!toolCalls) {
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
+        break;
+      }
+      for (const tool of toolCalls) {
+        const functionName = tool.function.name;
+        const functionArgs = tool.function.arguments;
+
+        let result = "";
+        if (functionName === "getTotalExpense") {
+          result = getTotalExpense(JSON.parse(functionArgs));
+        } else if (functionName === "addExpense") {
+          result = addExpense(JSON.parse(functionArgs));
+        }
+        messages.push({
+          role: "tool",
+          content: result,
+          tool_call_id: tool.id,
+        });
+      }
     }
   }
+  rl.close()
 }
 
 callAgent();
 
 // get total expense
 function getTotalExpense({ from, to }) {
-  console.log("Calling getTotalExpense tool");
-  // In reality db will be called
   const expense = expenseDB.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
@@ -108,7 +119,6 @@ function getTotalExpense({ from, to }) {
 }
 
 function addExpense({ name, amount }) {
-  console.log(`Adding ${amount} to expense db for ${name}`);
   expenseDB.push({ name, amount });
   return "Added to the database";
 }
